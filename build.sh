e5961bc2714174e3c5125b8d178a85e5af97afe2
Kevin.Wen <listream@126.com>
2017-11-13 19:59:44 +0800

    mlalgorithm, fix build flag

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-CC="gcc -g3 -O0 -Wall -m32 "
+CC="g++ -g3 -O0 -Wall "
 CC="$CC -I naivebayes"
 
 mkdir -p objs
@@ -11,5 +11,5 @@ $CC -c naivebayes/naivebayes.cpp -o objs/naivebayes.o
 
 ar rcs objs/libml.a objs/*.o
 
-gcc -m32 -g -Wall mlalgorithm_test.cpp -o objs/mlalgorithm_test objs/libml.a
+g++ -g -Wall mlalgorithm_test.cpp -o objs/mlalgorithm_test objs/libml.a
 
